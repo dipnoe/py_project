8089362044427235e833dc1ffa0c3a58b6703ff3
Elena Vagner <dipnoe@gmail.com>
2023-03-08 15:17:49 +0300

    add first function

--- funcs.py
+++ funcs.py
@@ -2,9 +2,3 @@ def add_nums(num1: int, num2: int) -> int:
     """ function adds two numbers
     """
     return num1 + num2
-
-
-def subtracts_nums(num1: int, num2: int) -> int:
-    """ function subtracts two numbers
-    """
-    return num1 - num2
